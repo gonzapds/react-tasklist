3372cb4f2a7ef949fff7c52a73cbf3b65335c9df
Gonzalo Pereira <pereiradossantosgonzalo@gmail.com>
2022-09-06 18:53:49 -0300

    componente de tareas agregado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.svg'
 import './App.css';
+import Tarea from './componentes/Tarea'
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='freecodecamp-logo-contenedor'>
+        <img 
+          src={freeCodeCampLogo} 
+          className='freecodecamp-logo'
+          alt='logo de freecodecamp' />
+      </div>
+      <div className='tareas-lista-principal'>
+        <h1>Mis Tareas</h1>
+        <Tarea texto='Aprender React' />
+      </div>
     </div>
   );
 }
